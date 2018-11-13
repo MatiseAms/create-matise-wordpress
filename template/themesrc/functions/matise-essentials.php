f8cd6085ab4b8805b099fa81665a97509d0ce268
Stephan Hoogland <stephan@shoogland.com>
2018-11-13 17:54:07 +0100

    add epic mailgun settings in code

--- template/themesrc/functions/matise-essentials.php
+++ template/themesrc/functions/matise-essentials.php
@@ -9,13 +9,15 @@ function debug($object){
 	echo '</pre>';
 }
 
-function remove_menu_links() {
-	// remove_menu_page('upload.php'); //remove media
-
+// stop here if DEVENV is not set
+if (!defined('DEVENV')){
+	return;
 }
 
-if (defined('DEVENV')) {
-	if(DEVENV === 'local' || DEVENV === 'staging'){
+// allow api access from anywhere for local and staging 
+switch (DEVENV) {
+	case 'local':
+	case 'staging':
 		update_option( 'upload_path', 'content/uploads' );
 		// update_option( 'upload_url_path', 'live content/uploads url to make images work' );
 
@@ -46,6 +48,47 @@ if (defined('DEVENV')) {
 			$plugins_to_deactivate
 		);
 
+		function remove_menu_links() {
+			// remove_menu_page('upload.php'); //remove media
+		
+		}
 		add_action( 'admin_menu', 'remove_menu_links' );
-	}
+		break;
+}
+
+// allow api access from anywhere for local and staging 
+switch (DEVENV) {
+	case 'local':
+	case 'staging':
+		header("Access-Control-Allow-Origin: *");
+		break;
+}
+
+// mailgun temporary matise settings
+switch (DEVENV) {
+	case 'staging':
+		update_option('mailgun', array(
+			'region' => 'us',
+			'useAPI' => 1,
+			'domain' => 'mg.matise.nl',
+			'apiKey' => 'key-caa5dc6f0a22f670cbebe427e6d20909',
+			'secure' => 1,
+			'sectype' => 'tls',
+			'track-clicks' => 'htmlonly',
+			'track-opens' => 1,
+			'from-address' => 'temp@mg.matise.nl',
+			'from-name' => 'Matise Staging Wordpress',
+			'campaign-id' => '<%= name %>'
+		));	
+
+		function general_admin_notice(){
+			global $pagenow;
+			if ( $pagenow == 'options-general.php' && isset($_GET['page']) && $_GET['page'] == 'mailgun') {
+				 echo '<div class="notice notice-warning is-dismissible">
+					 <p>Mailgun settings are defined in code on local and staging.</p>
+				 </div>';
+			}
+		}
+		add_action('admin_notices', 'general_admin_notice');
+		break;
 }
